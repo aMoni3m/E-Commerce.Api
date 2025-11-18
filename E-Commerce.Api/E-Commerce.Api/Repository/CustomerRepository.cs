29bcf1af563a0ef1b0bcd1ab83815a55972584a6
aMoni3m <abdulrahman2111656@miuegypt.edu.eg>
2025-11-18 10:54:52 +0200

    create repository for customer

---
+++ E-Commerce.Api/E-Commerce.Api/Repository/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using E_Commerce.Api.Data;
+using E_Commerce.Api.Models;
+using E_Commerce.Api.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Api.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> GetCustomerByEmail(string email)
+        {
+            return await _context.Customers.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        }
+
+        public async Task Add(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ E-Commerce.Api/E-Commerce.Api/Repository/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Api.Models;
+
+namespace E_Commerce.Api.Repository.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<bool> GetCustomerByEmail(String email);
+
+        Task Add(Customer customer);
+    }
+}
