c30ff97a04d558b63479faf21f8ed49bc14950e9
aMoni3m <abdulrahman2111656@miuegypt.edu.eg>
2025-11-20 18:26:55 +0200

    test crud operation in address controller

---
+++ E-Commerce.Api/E-Commerce.Api/Controllers/AddressController.cs
@@ -0,0 +1,51 @@
+﻿using E_Commerce.Api.DTOs.AdressDTOs;
+using E_Commerce.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly IAddressService _addressService;
+
+        public AddressController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllAddresses()
+        {
+            var response = await _addressService.AllAddressesAsync();
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAddress([FromBody] AddressCreateDTO addressCreateDTO)
+        {
+            var response = await _addressService.CreateAddressAsync(addressCreateDTO);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAddress([FromBody] AddressUpdateDTO addressUpdateDTO)
+        {
+            var response = await _addressService.UpdateAddressAsync(addressUpdateDTO);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAddress([FromBody] AddressDeleteDTO addressDeleteDTO)
+        {
+            var response = await _addressService.DeleteAddressAsync(addressDeleteDTO);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
