1fb91b601b7f64fc0acc814999ee27662140e6ab
aMoni3m <abdulrahman2111656@miuegypt.edu.eg>
2025-11-20 18:23:57 +0200

    add repository for address module

---
+++ E-Commerce.Api/E-Commerce.Api/Repository/AddressRepository.cs
@@ -0,0 +1,48 @@
+﻿using E_Commerce.Api.Data;
+using E_Commerce.Api.Models;
+using E_Commerce.Api.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Api.Repository
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AddressRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAddressAsync(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAddressAsync(Address address)
+        {
+            _context.Addresses.Remove(address);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Address> GetAddressByIdAsync(int customerId, int addressId)
+        {
+            Address? address = await _context.Addresses.FirstOrDefaultAsync(add => add.CustomerId == customerId && add.Id == addressId);
+            return address;
+        }
+
+        public async Task<List<Address>> GetAllAddressesAsync()
+        {
+            List<Address> addresses = await _context.Addresses.AsNoTracking().ToListAsync();
+
+            return addresses;
+        }
+
+        public async Task UpdateAddressAsync(Address address)
+        {
+            _context.Addresses.Update(address);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ E-Commerce.Api/E-Commerce.Api/Repository/Interfaces/IAddressRepository.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce.Api.DTOs.AdressDTOs;
+using E_Commerce.Api.Models;
+
+namespace E_Commerce.Api.Repository.Interfaces
+{
+    public interface IAddressRepository
+    {
+        Task CreateAddressAsync(Address address);
+
+        Task UpdateAddressAsync(Address address);
+
+        Task<Address> GetAddressByIdAsync(int customerId, int addressId);
+
+        Task<List<Address>> GetAllAddressesAsync();
+
+        Task DeleteAddressAsync(Address Adrress);
+    }
+}
