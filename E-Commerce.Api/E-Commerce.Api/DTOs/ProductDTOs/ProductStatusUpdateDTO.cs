96438bb3f03f32188711cb91e40a31a9f2056e85
aMoni3m <ar.moni3m53@gmail.com>
2025-12-01 23:17:54 +0200

    product dtos

---
+++ E-Commerce.Api/E-Commerce.Api/DTOs/ProductDTOs/ProductStatusUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Api.DTOs.ProductDTOs
+{
+    public class ProductStatusUpdateDTO
+    {
+        [Required(ErrorMessage = "ProductId is required.")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "IsAvailable is required.")]
+        public bool IsAvailable { get; set; }
+    }
+}
